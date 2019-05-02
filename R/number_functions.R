8010e7995e918cd850c244a3182ea7ca13444397
andrewjameshaynes <andrewjameshaynes@gmail.com>
2019-05-02 22:47:55 +0100

    added integer_reverse function

--- R/number_functions.R
+++ R/number_functions.R
@@ -29,3 +29,31 @@ x + (base - x %% base)
 round_down <-function(x, base){
   x - (x %% base)
 }
+
+
+#' @name integer_reverse
+#' @rdname integer_reverse
+#'
+#' @title integer_reverse
+#'
+#' @description Reverse an integer in place
+#'
+#' @param x A integer or numeric that can be coerced to an integer
+#' @details Reverses an integer x in place, for both positive and negative values. Does not work for vectors or non-integer data such as floats
+#' @return A rounded integer value
+#' @examples
+#'integer_reverse(123)
+#' [1] 321
+#' integer_reverse(-123)
+#' [1] -321
+integer_reverse = function(x){
+  if(all(x%%1 == 0 & length(x) == 1))
+    if(sign(x)==1)
+      as.integer(collapse(rev(strtok(x))))
+    else
+      -as.integer(collapse(rev(strtok(abs(x)))))
+  else
+    stop("Input is not a single integer")
+}
+
+
